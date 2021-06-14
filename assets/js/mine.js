3f17cbb568d4124cd71ff60add3a4a3fbc6f71ca
Coopster <samtonycooper@gmail.com>
2021-06-14 19:25:46 +0100

    more mobile support + minor changes

--- assets/js/mine.js
+++ assets/js/mine.js
@@ -3,3 +3,9 @@ document.getElementById("wip-banner")
 
   document.getElementById("wip-banner").id = "wip-banner-fade-out";
 });
+
+document.getElementById("wip-banner-mobile")
+        .addEventListener("click", function() {
+
+  document.getElementById("wip-banner-mobile").id = "wip-banner-fade-out-mobile";
+});
